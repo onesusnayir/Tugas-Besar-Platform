dbe9271f92a302d383d3dd1d750ccca68a747fdb
onesusnayir <senoriyan22@gmail.com>
2025-05-31 17:25:07 +0700

    Edit Review & Delete Review

--- app/src/main/java/com/example/manchingu/activities/ComicDetailActivity.java
+++ app/src/main/java/com/example/manchingu/activities/ComicDetailActivity.java
@@ -11,6 +11,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -52,6 +53,7 @@ public class ComicDetailActivity extends AppCompatActivity implements View.OnCli
     private ApiService apiService;
     private String token;
     private String idComic;
+    private String title;
     private SharedPreferences prefs;
     private List<BookmarkResponse.Comic> comicList = new ArrayList<>();
     private List<ReviewResponse.ReviewData> reviewList = new ArrayList<>();
@@ -65,9 +67,11 @@ public class ComicDetailActivity extends AppCompatActivity implements View.OnCli
     private TextView tvAvgRating;
     private TextView tvComment;
     private ImageView tvColorStatus;
-
-
-
+    private LinearLayout postUserReview, getUserReview;
+    private Button deleteReviewBtn, editReviewBtn;
+    private String reviewId;
+    private ReviewResponse.ReviewData reviewData;
+    private TextView updateBtn, tvPosting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,7 +84,7 @@ public class ComicDetailActivity extends AppCompatActivity implements View.OnCli
         });
 
         // Ambil data dari Intent
-        String title = getIntent().getStringExtra("title");
+        title = getIntent().getStringExtra("title");
         String author = getIntent().getStringExtra("author");
         String artist = getIntent().getStringExtra("artist");
         String description = getIntent().getStringExtra("synopsis");
@@ -109,6 +113,8 @@ public class ComicDetailActivity extends AppCompatActivity implements View.OnCli
         TextView tvStatus = findViewById(R.id.tvStatus);
         TextView tvBookmarkNum = findViewById(R.id.tvBookmarkNum);
         tvColorStatus = findViewById(R.id.tvColorStatus);
+        postUserReview = findViewById(R.id.post_user_review);
+        getUserReview = findViewById(R.id.get_user_review);
         
         // Inisialisasi Edit Text & Rating Bar
         etUlasan = findViewById(R.id.et_ulasan);
@@ -165,13 +171,67 @@ public class ComicDetailActivity extends AppCompatActivity implements View.OnCli
 
         backBtn = findViewById(R.id.back_btn);
         backBtn.setOnClickListener(this);
-
-        TextView tvPosting = findViewById(R.id.tv_posting);
+        tvPosting = findViewById(R.id.tv_posting);
         tvPosting.setOnClickListener(this);
 
+        // --- Reviews ---
+        // Edit & Delete Review
+        deleteReviewBtn = findViewById(R.id.delete_user_review);
+        deleteReviewBtn.setOnClickListener(this);
+
+        editReviewBtn = findViewById(R.id.edit_user_review);
+        editReviewBtn.setOnClickListener(this);
+
+        // Update Review Btn
+        updateBtn = findViewById(R.id.tv_update);
+
+        // Method untuk mendapatkan list bookmark: digunakan untuk mengetahui apakah
+        // ngebookmark comic ini
         getBookmarkList();
 
+        // Method untuk mendapatkan list review comic ini
         getReviewComics();
+
+        // Method untuk mengetahui apakah user mereview comic ini atau tidak
+        getUserReview();
+    }
+
+    private void getUserReview() {
+        apiService.getUserReview("Bearer "+token, title)
+            .enqueue(new Callback<ReviewResponse>() {
+                    @Override
+                    public void onResponse(Call<ReviewResponse> call, Response<ReviewResponse> response) {
+                        if (response.isSuccessful() && response.body() != null && response.body() != null) {
+                            List<ReviewResponse.ReviewData> items = response.body().getData();
+                            if (items.isEmpty()){
+                                postUserReview.setVisibility(View.VISIBLE);
+                                getUserReview.setVisibility(View.GONE);
+                            }else {
+                                reviewData = items.get(0);
+                                reviewId = items.get(0).getId_review();
+                                getUserReview.setVisibility(View.VISIBLE);
+                                postUserReview.setVisibility(View.GONE);
+
+                                TextView username, rating, reviewText, time;
+                                username = findViewById(R.id.tv_users_username);
+                                rating = findViewById(R.id.tv_users_rating);
+                                reviewText = findViewById(R.id.tv_users_review);
+                                time = findViewById(R.id.tv_users_time);
+
+                                username.setText("You");
+                                rating.setText(String.valueOf(items.get(0).getRating()));
+                                reviewText.setText(items.get(0).getReview_text());
+                                time.setText(items.get(0).getUpdated_at().split("T")[0]);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ReviewResponse> call, Throwable t) {
+
+                    }
+                }
+            );
     }
 
     @Override
@@ -203,7 +263,68 @@ public class ComicDetailActivity extends AppCompatActivity implements View.OnCli
             idComic = getIntent().getStringExtra("id_comic");
             postNewReview(reviewBody);
         }
+        // --- Delete Review ---
+        else if (v.getId() == R.id.delete_user_review) {
+            deleteUserReview();
+        }
+        // --- Edit Review
+        else if (v.getId() == R.id.edit_user_review) {
+            editUserReview();
+            tvPosting.setVisibility(View.GONE);
+            updateBtn.setVisibility(View.VISIBLE);
+        }
     }
+
+    private void editUserReview() {
+        postUserReview.setVisibility(View.VISIBLE);
+        getUserReview.setVisibility(View.GONE);
+
+        etUlasan.setText(reviewData.getReview_text());
+        ratingBar.setRating((float) reviewData.getRating());
+
+        TextView updateBtn = findViewById(R.id.tv_update);
+        updateBtn.setOnClickListener(v -> {
+            JsonObject jsonBody = new JsonObject();
+            jsonBody.addProperty("rating", (int) ratingBar.getRating());
+            jsonBody.addProperty("review_text", etUlasan.getText().toString().trim());
+
+            apiService.updateUserReview("Bearer "+token, reviewId, jsonBody)
+                .enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        if (response.isSuccessful() && response.body() != null && response.body() != null) {
+                            String message = response.body().get("message").getAsString();
+                            Toast.makeText(ComicDetailActivity.this, message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+
+                    }
+                });
+        });
+
+    }
+
+    private void deleteUserReview() {
+        apiService.deleteUserReview("Bearer "+token, reviewId)
+            .enqueue(new Callback<JsonObject>() {
+                @Override
+                public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                    if (response.isSuccessful() && response.body() != null && response.body() != null) {
+                        String message = response.body().get("message").getAsString();
+                        Toast.makeText(ComicDetailActivity.this, message, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<JsonObject> call, Throwable t) {
+
+                }
+            });
+    }
+
     private void getReviewComics() {
         apiService.getComicReviews("Bearer "+token, idComic)
             .enqueue(new Callback<ReviewResponse>() {
